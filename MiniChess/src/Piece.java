c6e4cb52577edf11d1c602ffeae8749713e7f9e3
Craig Meinschein <cmeinsch@gmail.com>
2013-05-07 21:38:17 -0700

    Added evaluation of if a major piece is outside its starting square.

--- MiniChess/src/Piece.java
+++ MiniChess/src/Piece.java
@@ -5,7 +5,61 @@
  *   them simpler to work with.
  */
 public class Piece {
-
+		private char piece;       // The character representation of this piece.
+		private Square position;  // The square that this piece resides on.
+		
+		public Piece() {
+			piece = 'P';
+			position = new Square(0,1);
+		}
+		
+		public Piece(char newpiece, Square newpos) {
+			piece = newpiece;
+			position = new Square(newpos.x, newpos.y);
+		}
+		
+		public Piece(char newpiece, int x, int y) {
+			piece = newpiece;
+			position = new Square(x,y);
+		}
+		
+		/* Function:
+		 *   isDeveloped
+		 * Description:
+		 *   Returns true if this piece is off of its home square.
+		 * Inputs:
+		 *   None.
+		 * Return values:
+		 *    True : Returned if this piece is off of the square it starts in in a new game of MiniChess.
+		 *   False : Returned if this piece is in the same square it starts in in a new game of MiniChess.
+		 */
+		public boolean isDeveloped() {
+			switch(piece) {
+			case 'K':
+				return !(position.x == 4 && position.y == 0); // x=4 y=0
+			case 'Q':
+				return !(position.x == 3 && position.y == 0); // x=3 y=0
+			case 'B':
+				return !(position.x == 2 && position.y == 0); // x=2 y=0
+			case 'N':
+				return !(position.x == 1 && position.y == 0); // x=1 y=0
+			case 'R':
+				return !(position.x == 0 && position.y == 0); // x=0 y=0
+			case 'k':
+				return !(position.x == 0 && position.y == 5); // x=0 y=5
+			case 'q':
+				return !(position.x == 1 && position.y == 5); // x=1 y=5
+			case 'b':
+				return !(position.x == 2 && position.y == 5); // x=2 y=5
+			case 'n':
+				return !(position.x == 3 && position.y == 5); // x=3 y=5
+			case 'r':
+				return !(position.x == 4 && position.y == 5); // x=4 y=5
+			default:
+				return false;
+			}
+		}
+		
 		/* Function:
 		 *   isWhite
 		 * Description:
@@ -21,6 +75,20 @@ public class Piece {
 		}
 		
 		/* Function:
+		 *   isWhite
+		 * Description:
+		 *   Returns true if this piece is White, false if not.
+		 * Inputs:
+		 *   None.
+		 * Return values:
+		 *    True : Returned if this piece is White (upper case).
+		 *   False : Returned if this piece is Black (lower case).
+		 */
+		public boolean isWhite() {
+			return Character.isUpperCase(piece);
+		}
+		
+		/* Function:
 		 *   isBlack
 		 * Description:
 		 *   Returns true if the given piece is Black, false if not.
@@ -32,5 +100,19 @@ public class Piece {
 		 */
 		public static boolean isBlack(char ch) {
 			return Character.isLowerCase(ch);
+		}
+		
+		/* Function:
+		 *   isBlack
+		 * Description:
+		 *   Returns true if this piece is Black, false if not.
+		 * Inputs:
+		 *   None.
+		 * Return values:
+		 *    True : Returned if this piece is Black (lower case).
+		 *   False : Returned if this piece is White (upper case).
+		 */
+		public boolean isBlack() {
+			return Character.isLowerCase(piece);
 		}
 }
