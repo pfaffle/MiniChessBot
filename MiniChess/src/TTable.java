cfd51afeb4f5e1519c70dc47bcebf6010fefcff0
Craig Meinschein <cmeinsch@gmail.com>
2013-06-05 20:40:00 -0700

    Implemented fetching/storing functions for TTables.

--- MiniChess/src/TTable.java
+++ MiniChess/src/TTable.java
@@ -7,9 +7,36 @@
 public class TTable {
 
 	public TTableEntry[] entries;
+	private int numEntries;
 	
 	public TTable() {
-		TTableEntry[] entries = new TTableEntry[256];
+		numEntries = 256;
+		entries = new TTableEntry[numEntries];
 	}
 	
+	/* Takes a hash and gives the TTable entry that it corresponds to,
+	 * if it exists. Returns null if the entry is not valid. */
+	public TTableEntry getEntry(long tgtHash) {
+		int tgtIndex = getIndex(tgtHash);
+		if (entries[tgtIndex].valid)
+			return entries[tgtIndex];
+		
+		return null;
+	}
+	
+	/* Stores a new TTable Entry into the TTable. */
+	public void storeEntry(TTableEntry newEntry, long newHash) {
+		newEntry.valid = true;
+		int tgtIndex = getIndex(newHash);
+		entries[tgtIndex] = newEntry;
+	}
+	
+	/* Takes a hash and returns the array index in the TTable
+	 * that it corresponds to. */
+	private int getIndex(long tgtHash) {
+		// 256 is 2^8 (8 bits), long is 64 bits, mask 56 bits.
+		for (int i = 0; i < 56; i++)
+			tgtHash = tgtHash >> 1;
+		return (int)tgtHash;
+	}
 }
